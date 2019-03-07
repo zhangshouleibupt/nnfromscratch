9405470957bfdbae33089ec735b83554470e0668
shoulei <zhangshouleibupt@163.com>
2019-03-07 20:25:27 +0800

    write a gradient descent use autograd

--- nn/rnn.py
+++ nn/rnn.py
@@ -1,1 +1,15 @@
 import torch
+from torch.autograd import Variable
+
+t = Variable(torch.randn(1,2),requires_grad=True)
+
+epochs = 1000
+lr = 0.01
+for epoch in range(epochs):
+	y = torch.sum(t**2)/2
+	y.backward()
+	t.data -= lr * t.grad.data
+	print(t)
+	if y.item() < 0.1:
+		lr = 0.001
+		print(lr)
