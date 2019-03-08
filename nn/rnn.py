0cff722bef65c07c2b0dd3ac033d03b7302cc5a3
shoulei <zhangshouleibupt@163.com>
2019-03-08 17:09:00 +0800

    refresh

--- nn/rnn.py
+++ nn/rnn.py
@@ -1,7 +1,7 @@
 import torch
 from torch.autograd import Variable
-
-
+import numpy as np
+import math
 # standard gradient descent
 t = Variable(torch.randn(1,2),requires_grad=True)
 epochs = 1000
@@ -46,3 +46,34 @@ hidden_size = 6
 output_size = 1
 lr = 0.01
 
+#a polynomial curve fitting 
+class CurveFitting():
+	def __init__(self,x_set,y_set,M=3):
+		self.M = M
+		self.x_set = x_set
+		self,y_set = y_set
+		self.w = Variable(torch.randn(1,M),requires_grad=True)
+		#self.loss = feeddata()
+	def feeddata(self):
+		loss = 0
+		for x,y in zip(self.x_set,self.y_set):
+			x_boost = self.custome(x)
+			out = x_boost.mm(self.w)
+			current_loss = (out - y).pow(2).sum()/2
+			loss += current_loss
+		return loss
+	def custome(self,x):
+		x = [torch.tensor(x).pow(i).item() for i in range(self.M)]
+		return torch.tensor(x)
+	def gradientDescent(self,lr=0.01,epochs=100):
+		loss = self.feeddata()
+		for epoch in range(epochs):
+			loss.backward()
+			self.w.data -= lr * self.w.grad.data
+			loss = self.feeddata()
+			
+seq_length = 20
+data_time_steps = np.linspace(0, 1, seq_length + 1)
+data = np.sin(2*math.pi*data_time_steps)
+print(data_time_steps)
+print(data)
